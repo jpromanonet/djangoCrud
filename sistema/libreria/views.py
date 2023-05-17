d5e9bbc8537415f51150074a30befe6320405a2a
jpromanonet <programming@jpromano.net>
2023-05-17 17:40:36 -0300

    Us section

--- sistema/libreria/views.py
+++ sistema/libreria/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def inicio(request):
     return HttpResponse("<h1>Hello World</h1>")
+
+def us(request):
+    return render(request, 'pages/us.html')
