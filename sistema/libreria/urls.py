0856f1ed4049aaa1ff6b545256a1539b8e85fd4c
jpromanonet <programming@jpromano.net>
2023-05-18 18:54:14 -0300

    Index books section

--- sistema/libreria/urls.py
+++ sistema/libreria/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
-    path('us', views.us, name='us'),
+    path('', views.index, name='index'),
 ]
--- sistema/libreria/views.py
+++ sistema/libreria/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def inicio(request):
-    return HttpResponse("<h1>Hello World</h1>")
-
-def us(request):
-    return render(request, 'pages/us.html')
+def index(request):
+    return render(request, 'books/index.html')
