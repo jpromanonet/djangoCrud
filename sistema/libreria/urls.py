eee480b3cb94021514bf7bc705b27387e01180d6
jpromanonet <programming@jpromano.net>
2023-05-19 22:50:05 -0300

    Index books section

--- sistema/libreria/urls.py
+++ sistema/libreria/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('us', views.us, name='us'),
-    path('books', views.books, name="books")
+    path('books', views.books, name="books"),
     path('addBooks', views.books, name="addBooks")
 ]
