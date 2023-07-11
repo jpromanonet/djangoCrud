99af23fe2f4da7bd26d5b17f2f90c91aea4eed18
jpromanonet <programming@jpromano.net>
2023-07-11 20:55:45 -0300

    Customizing Django admin

--- sistema/libreria/models.py
+++ sistema/libreria/models.py
@@ -6,3 +6,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100, verbose_name='Title')
     image = models.ImageField(upload_to='images/', verbose_name='Image', null=True)
     description = models.TextField(verbose_name='Description', null=True)
+
+    def __str__(self):
+        row = 'Title: ' + self.title + ' - ' + 'Description: ' + self.description
+        return row
