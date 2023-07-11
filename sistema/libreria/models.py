8f6cdacac3a9b3db5a40423b4f685b0c6ad288c2
jpromanonet <programming@jpromano.net>
2023-07-11 20:25:02 -0300

    Models creation

--- sistema/libreria/models.py
+++ sistema/libreria/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Book(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100, verbose_name='Title')
-    image = models.ImageField(upload_to='images/', null=True)
-    description = models.TextField(null=True)
+    image = models.ImageField(upload_to='images/', verbose_name='Image', null=True)
+    description = models.TextField(verbose_name='Description', null=True)
